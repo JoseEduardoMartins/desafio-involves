738f93149bfb195f3a69eb82526d0d8f2cee1fb4
José Eduardo Martins <m4rt1ns.jose@gmail.com>
2021-05-01 23:29:54 -0300

    projeto

---
+++ desafioInvolves.js
@@ -0,0 +1,73 @@
+let hotel = [
+    {
+        name: "Parque das flores",
+        classification: 3,
+        weekdayRegular: 110.00,
+        weekendRegular: 90.00,
+        weekdayFidelity: 80.00,
+        weekendFidelity: 80.00
+    },
+    {
+        name: "Jardim Botânico",
+        classification: 4,
+        weekdayRegular: 160.00,
+        weekendRegular: 60.00,
+        weekdayFidelity: 110.00,
+        weekendFidelity: 50.00
+    },
+    {
+        name: "Mar Atlântico",
+        classification: 5,
+        weekdayRegular: 220.00,
+        weekendRegular: 150.00,
+        weekdayFidelity: 100.00,
+        weekendFidelity: 40.00
+    }
+];
+function gethotelCheaper(typeClient, ...date){
+    let valueTotal = 99999999;
+    let nameHotel = "";
+    let classificationHotel = 0;
+    let weekDays = ["mon", "tue", "wed", "thu", "fri"];
+    for(var numbemHotel = 0; numbemHotel < hotel.length; numbemHotel++){
+        let value = calcValeuTotal(date, weekDays, typeClient, numbemHotel);
+        if(value < valueTotal){
+            valueTotal = value;
+            nameHotel = hotel[numbemHotel].name;
+            classificationHotel = hotel[numbemHotel].classification;
+            
+        }else if(value == valueTotal){
+            if(classificationHotel < hotel[numbemHotel].classification){
+                valueTotal = value;
+                nameHotel = hotel[numbemHotel].name;
+                classificationHotel = hotel[numbemHotel].classification;
+            }
+        }
+    }
+    return nameHotel;
+}
+function calcValeuTotal(date, weekDays, typeClient, numbemHotel){
+    let value = 0;
+    for(let i = 0; i < date.length; i++){
+        let data = date[i].match(/\(.*\)/);
+        data = data[0].replace("(", "");
+        data = data.replace(")", "");
+        for(var k = 0; k < weekDays.length; k++){
+            if(data == weekDays[k]){
+                (typeClient == "Regular") ? value += hotel[numbemHotel].weekdayRegular: value += 0;
+                (typeClient == "Fidelidade") ? value += hotel[numbemHotel].weekdayFidelity: value += 0;
+            }
+        }
+        if(typeClient == "Regular"){
+            (data == "sat") ? value += hotel[numbemHotel].weekendRegular : value += 0;
+            (data == "sun") ? value += hotel[numbemHotel].weekendRegular : value += 0;
+        }else if (typeClient == "Fidelidade"){
+            (data == "sat") ? value += hotel[numbemHotel].weekendFidelity : value += 0;
+            (data == "sun") ? value += hotel[numbemHotel].weekendFidelity : value += 0;
+        }
+    }
+    return value;
+}
+console.log(gethotelCheaper("Regular", "16Mar2020(mon)", "17Mar2020(tue)", "18Mar2020(wed)"));
+console.log(gethotelCheaper("Regular", "20Mar2020(fri)", "21Mar2020(sat)", "22Mar2020(sun)"));
+console.log(gethotelCheaper("Fidelidade", "26Mar2020(thu)", "27Mar2020(fri)", "28Mar2020(sat)"));
